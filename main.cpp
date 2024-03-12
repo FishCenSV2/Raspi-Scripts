efcf731e34575ab6893cbdc15eeefbed8d6c2767
JFSXD <156885170+JFSXD@users.noreply.github.com>
2024-03-11 17:43:29 -0700

    Update main.cpp
    added repeated camera trying, off button (in the form of a .txt), and GPIO control for indicator lights (via bash)

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 const int repeats = 100;
-const int ms_length = 10000; //60000 = 1 minute
+const int ms_length = 900000; //60000 = 1 minute
 
 //-exec function obtained from working-solution at https://stackoverflow.com/questions/52164723/how-to-execute-a-command-and-get-return-code-stdout-and-stderr-of-command-in-c
 string exec(const char* cmd) {
@@ -27,10 +27,10 @@ string exec(const char* cmd) {
     auto rc = pclose(pipe);
 
     if (rc == EXIT_SUCCESS) {
-        cout << "SUCCESS\n";
+        cout << "BASH SUCCESS\n";
     }
     else {
-        cout << "FAILED\n";
+        cout << "BASH FAILED\n";
     }
 
     return result;
@@ -50,12 +50,12 @@ int main()
     fstream log;
     log.open("/media/FishCenS/KINGSTON/cam_log.txt", ios::out | ios::in | ios::app | ios::binary);
     if (!log.is_open()) {
-        cout << "something wrong with file" << endl;
+        cout << "something wrong with cam_log file" << endl;
         log.close();
         return 0;
     }
     else {
-        cout << "file opened" << endl;
+        cout << "cam_log file opened" << endl;
         log.seekg(-1, ios::end);
         while (log.peek() == '\n') {
             log.seekg(-1, ios::cur);
@@ -69,23 +69,80 @@ int main()
         log.close();
     }
 
+    log.open("/media/FishCenS/KINGSTON/111_OFF_BUTTON.txt", ios::trunc | ios::out);
+    if(!log.is_open()) {
+        cout << "something wrong with off button" << endl;
+        log.close();
+        return 0;
+    }
+    else {
+        cout << "off button created" << endl;
+        log << "running";
+        log.close();
+    }
+
+    string gpioSetup = "";
+    gpioSetup += "echo \"15\" > /sys/class/gpio/export && "; //enable GPIO15 (YLW LED)
+    gpioSetup += "echo \"18\" > /sys/class/gpio/export && "; //enable GPIO18 (RED LED)
+    gpioSetup += "echo \"out\" > /sys/class/gpio/gpio15/direction && "; //set GPIO15 as output
+    gpioSetup += "echo \"out\" > /sys/class/gpio/gpio18/direction"; //set GPIO18 as output
+    res = exec(gpioSetup.c_str());
+
+    const string YLW_ON = "echo \"1\" > /sys/class/gpio/gpio15/value";
+    const string YLW_OFF = "echo \"0\" > /sys/class/gpio/gpio15/value";
+    const string RED_ON = "echo \"1\" > /sys/class/gpio/gpio18/value";
+    const string RED_OFF = "echo \"0\" > /sys/class/gpio/gpio18/value";
+
+    bool running, exitbutton = false, redstate = false;
 
     for (int i = 1; i <= repeats; i++) {
-        fname = dateandtime + to_string(i);
-        cout << fname << endl;
-        strcmd = "cd /media/FishCenS/KINGSTON && ls";
-        strcmd += " && libcamera-vid -t " + to_string(ms_length) + " -o " + fname + "-raw.h264 --save-pts " + fname + "-stamps.txt";
-        strcmd += " 2>&1";
-        //strcmd += " &>> /media/FishCenS/KINGSTON/" + dateandtime + "_log.txt";
-        //strcmd += " && mkvmerge -o " + fname + "-vid.mkv --timecodes 0:" + fname + "-stamps.txt " + fname + "-raw.h264";
-        //-cmd code adapted from example code in libcamera-vid section at https://www.raspberrypi.com/documentation/computers/camera_software.html
-        cout << strcmd << endl;
-        res = exec(strcmd.c_str());
-        cout << res << endl;
-
+        res = exec(YLW_ON.c_str());
+        running = false;
+        while (!running) {
+            res = exec(redstate ? RED_ON.c_str() : RED_OFF.c_str());
+            redstate = !redstate;
+            fname = dateandtime + to_string(i);
+            cout << fname << endl;
+            strcmd = "cd /media/FishCenS/KINGSTON && ls"; //move to directory of USB drive
+            strcmd += " && libcamera-vid -t " + to_string(ms_length) + " --framerate 60 -o " + fname + "-raw.h264 --save-pts " + fname + "-stamps.txt";
+                //save video (plus timestamps for later conversion to .mp4)
+            strcmd += " 2>&1"; //save stdout and stderr of bash command for res variable
+            //strcmd += " && mkvmerge -o " + fname + "-vid.mkv --timecodes 0:" + fname + "-stamps.txt " + fname + "-raw.h264"; //conversion to .mp4
+                //excluded due to time loss
+            //-cmd code adapted from example code in libcamera-vid section at https://www.raspberrypi.com/documentation/computers/camera_software.html
+            cout << strcmd << endl;
+            res = exec(strcmd.c_str());
+            cout << res << endl;
+            running = (res.find("failed") == std::string::npos); //check if something went wrong
+            if (!running) { //if something went wrong, put up error lights
+                res = exec(RED_ON.c_str());
+                res = exec(YLW_OFF.c_str());
+                redstate = true;
+            }
+            log.open("/media/FishCenS/KINGSTON/111_OFF_BUTTON.txt", ios::in); //check off button
+            if (!log.is_open()) { //off button deleted
+                cout << "OFF BUTTON DISCONNECTED";
+                log.close();
+                exitbutton = true;
+                break;
+            }
+            else {
+                string check;
+                log >> check;
+                cout << check << endl;
+                log.close();
+                if (check.find("running") == std::string::npos) { //off button activated
+                    cout << "OFF BUTTON ACTIVATED";
+                    exitbutton = true;
+                    break;
+                    }
+            }
+        }
+        if (exitbutton)
+            break;
         log.open("/media/FishCenS/KINGSTON/" + dateandtime + "-log.txt", ios::out | ios::app | ios::binary);
         if (!log.is_open()) {
-            cout << "something wrong with file" << endl;
+            cout << "something wrong with current log file" << endl;
             log.close();
             return 0;
         }
@@ -96,7 +153,7 @@ int main()
 
         log.open("/media/FishCenS/KINGSTON/cam_log.txt", ios::out | ios::app | ios::binary);
         if (!log.is_open()) {
-            cout << "something wrong with file" << endl;
+            cout << "something wrong with cam_log file" << endl;
             log.close();
             return 0;
         }
@@ -106,5 +163,12 @@ int main()
         }
         //cout << strcmd << endl;
     }
+
+    gpioSetup = "";
+    gpioSetup += "echo \"0\" > /sys/class/gpio/gpio15/value && "; //turn off GPIO15
+    gpioSetup += "echo \"0\" > /sys/class/gpio/gpio18/value && "; //turn off GPIO18
+    gpioSetup += "echo \"15\" > /sys/class/gpio/unexport && "; //disable GPIO15
+    gpioSetup += "echo \"18\" > /sys/class/gpio/unexport"; //disable GPIO18
+    res = exec(gpioSetup.c_str());
 
 }
